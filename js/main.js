6f0c9442f9a3d967c9b322959959f6e756416d5a
Gabriela Perez <gabrielaperez1127@gmail.com>
2017-01-14 20:06:59 -0500

    terminada traduccion

---
+++ js/main.js
@@ -0,0 +1,20 @@
+/*
+var boton = document.createElement("button");
+var contenido = document.createTextNode("Español");
+boton.appendChild(contenido);
+document.body.appendChild(boton);
+document.getElementByTagName("button")[1].setAttribute(" onclick", "español()"); 
+
+function español(){
+    
+}
+*/
+
+
+
+document.getElementById("form-signin-heading").innerHTML="Por favor inicie sesión";
+
+document.getElementById("inputEmail").placeholder="Correo Electrónico";
+document.getElementById("inputPassword").placeholder="Contraseña";
+document.querySelector("span").innerHTML="Recordar datos";
+document.querySelector("button").innerHTML="Iniciar Sesión";
